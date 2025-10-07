eb357a568aaa5f0b4ab92e850543de6497924469
nalongal <nalongal@myuax.com>
2025-10-07 12:24:19 +0200

    Create evaluation.py

---
+++ evaluation.py
@@ -0,0 +1,6 @@
+exam_score = float(input("Please, enter the exam score:"))
+assistance = float(input("Please, enter the assistance:"))
+if exam_score  >= 70 and assistance >= 80:
+    print("pass")
+else:
+    print(f"fail")
